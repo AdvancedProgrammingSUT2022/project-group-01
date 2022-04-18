6f96439e3d854b5d28c4d4b4fb3c0d69ecfd5a44
AmooSafar <30364725+AmooSafar@users.noreply.github.com>
2022-04-18 09:07:27 +0430

    just new branch

---
+++ src/controller/InfoMenuController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.technology.TechnologyType;
+
+public class InfoMenuController extends Controller {
+
+
+	public TechnologyType getCurrentResearch(){return null; }
+	public int getResearchRemainingTurn(){return 0; }
+
+
+}
---
+++ src/view/InfoMenu.java
@@ -0,0 +1,5 @@
+package view;
+
+public class InfoMenu extends Menu {
+	private InfoMenuController
+}
