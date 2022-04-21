f1c9753298e45b7f313a0cb90d32ed658985cdf6
Amirhossein Azizi <amirhosseinazizi10@gmail.com>
2022-04-21 20:27:44 +0430

    :D

---
+++ src/model/civilization/production/ProductionInventory.java
@@ -0,0 +1,42 @@
+package model.civilization.production;
+
+import model.civilization.Civilization;
+import java.util.*;
+
+public class ProductionInventory {
+
+	Civilization civilization;
+	private Vector<Production> productions;
+	private Production currentProduction;
+
+	private void createList() {
+		// TODO - implement model.civilization.production.ProductionInventory.createList
+		throw new UnsupportedOperationException();
+	}
+
+	public ProductionInventory() {
+		// TODO - implement model.civilization.production.ProductionInventory.model.civilization.production.ProductionInventory
+		throw new UnsupportedOperationException();
+	}
+
+	//true means producing is finished
+	public boolean nextTurn() {
+		// TODO - implement model.civilization.production.ProductionInventory.updateProduction
+		throw new UnsupportedOperationException();
+	}
+
+	public Object getCurrentProduction() {
+		// TODO - implement here
+		return null;
+	}
+
+	public Vector<Production> getAvailableProductions() {
+		// TODO - implement model.civilization.production.ProductionInventory.getAvailableProductions
+		throw new UnsupportedOperationException();
+	}
+
+	public void cancelCurrentProduction(){
+
+	}
+
+}
---
+++ src/view/LoginMenu.java
@@ -0,0 +1,23 @@
+package view;
+
+import controller.LoginMenuController;
+
+public class LoginMenu {
+
+	private LoginMenuController controller;
+
+	/**
+	 * 
+	 * @param controller
+	 */
+	public LoginMenu(LoginMenuController controller) {
+		// TODO - implement view.LoginMenu.view.LoginMenu
+		throw new UnsupportedOperationException();
+	}
+
+	public void run() {
+		// TODO - implement view.LoginMenu.run
+		throw new UnsupportedOperationException();
+	}
+
+}
