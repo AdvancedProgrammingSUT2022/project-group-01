4d6460f95392c9e136d55cbdd94538aa3dddca44
Amirhossein Azizi <amirhosseinazizi10@gmail.com>
2022-04-23 17:14:39 +0430

    adding model

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,10 @@
+package model;
+
+import model.civilization.Civilization;
+
+public class Player {
+
+	private User user;
+	private Civilization civilization;
+
+}
---
+++ src/main/java/model/resource/ResourceList.java
@@ -0,0 +1,15 @@
+package model.resource;
+
+import java.util.List;
+
+public class ResourceList {
+	private List<ResourceType> resources;
+
+	public ResourceList(ResourceType... resource) {
+		this.resources = List.of(resource);
+	}
+
+	public boolean isResearched() {
+		return true;
+	}
+}
